ef1577470503169468a2953fdc7ae53b31bd7b0f
Christian Jaena <christian.jaena@msugensan.edu.ph>
2020-07-08 00:00:18 +0800

    initial commit

---
+++ components/AddItem.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+
+const Additem = ({ addItem }) => {
+  const [text, setText] = useState('');
+
+  const onChange = value => {
+    setText(value);
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Add item..."
+        style={styles.input}
+        value={text}
+        onChangeText={onChange}
+      />
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => {
+          addItem(text);
+          setText('');
+        }}>
+        <Text style={styles.btnText}>
+          <Icon name="plus" size={20} />
+          Add Item
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    height: 60,
+    padding: 8,
+    fontSize: 16,
+  },
+  btn: {
+    backgroundColor: '#c2bad8',
+    padding: 9,
+    margin: 5,
+  },
+  btnText: {
+    color: 'darkslateblue',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
+export default Additem;
